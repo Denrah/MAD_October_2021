840df0bd54883a25f45cc37206bbe5e8d524bea2
Denis Sharapov <denisharapov@yandex.ru>
2021-10-31 17:45:04 +0700

    Add flying circles

--- TrainTest1/StartScreen/StartViewController.swift
+++ TrainTest1/StartScreen/StartViewController.swift
@@ -30,6 +30,7 @@ class CommonButton: UIButton {
 }
 
 class StartViewController: BaseViewController {
+  var cContainerView = UIView()
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -86,10 +87,16 @@ class StartViewController: BaseViewController {
       make.bottom.equalTo(accLabel.snp.top).offset(-24)
     }
     
+    view.addSubview(cContainerView)
+    cContainerView.snp.makeConstraints { make in
+      make.top.leading.trailing.equalToSuperview()
+      make.bottom.equalTo(signUpButton.snp.top)
+    }
     
     signUpButton.addTarget(self, action: #selector(signUp), for: .touchUpInside)
     signInButton.addTarget(self, action: #selector(signIn), for: .touchUpInside)
     
+    setupImages()
   }
   
   @objc func signUp() {
@@ -98,6 +105,63 @@ class StartViewController: BaseViewController {
   
   @objc func signIn() {
     navigationController?.pushViewController(SignInViewController(), animated: true)
+  }
+  
+  var timer: Timer?
+  
+  func setupImages() {
+    timer?.invalidate()
+    view.layoutIfNeeded()
+    cContainerView.subviews.forEach { $0.removeFromSuperview() }
+    for index in 1...6 {
+      let imageView = CCImageView(direction: CDirection.allCases.randomElement() ?? .leftTop)
+      imageView.image = UIImage(named: "c\(index)")
+      
+      let imX = CGFloat.random(in: 0...(UIScreen.main.bounds.width - 100))
+      let imY = CGFloat.random(in: 0...(cContainerView.frame.height - 100))
+      
+      imageView.frame = CGRect(x: imX, y: imY, width: 100, height: 100)
+      
+      cContainerView.addSubview(imageView)
+    }
+    
+    timer = Timer.scheduledTimer(withTimeInterval: 0.03, repeats: true, block: { _ in
+      self.updateImages()
+    })
+  }
+  
+  func updateImages() {
+    cContainerView.subviews.compactMap { $0 as? CCImageView }.forEach { imageView in
+      imageView.frame = CGRect(x: imageView.frame.origin.x + imageView.direction.dx,
+                               y: imageView.frame.origin.y + imageView.direction.dy,
+                               width: 100, height: 100)
+      
+      if imageView.frame.origin.x < 0 || imageView.frame.origin.x + 100 > cContainerView.frame.width {
+        switch imageView.direction {
+        case .leftBottom:
+          imageView.direction = .rightBottom
+        case .leftTop:
+          imageView.direction = .rightTop
+        case .rightBottom:
+          imageView.direction = .leftBottom
+        case .rightTop:
+          imageView.direction = .leftTop
+        }
+      }
+      
+      if imageView.frame.origin.y < 0 || imageView.frame.origin.y + 100 > cContainerView.frame.height {
+        switch imageView.direction {
+        case .leftBottom:
+          imageView.direction = .leftTop
+        case .leftTop:
+          imageView.direction = .leftBottom
+        case .rightBottom:
+          imageView.direction = .rightTop
+        case .rightTop:
+          imageView.direction = .rightBottom
+        }
+      }
+    }
   }
   
   
@@ -111,4 +175,52 @@ class StartViewController: BaseViewController {
    }
    */
   
+}
+
+class CCImageView: UIImageView {
+  override var intrinsicContentSize: CGSize {
+    return CGSize(width: 100, height: 100)
+  }
+  
+  var direction: CDirection
+  
+  init(direction: CDirection) {
+    self.direction = direction
+    super.init(frame: .zero)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+
+enum CDirection: CaseIterable {
+  case leftTop, leftBottom, rightTop, rightBottom
+  
+  var dx: CGFloat {
+    switch self {
+    case .leftTop:
+      return -2
+    case .leftBottom:
+      return -2
+    case .rightTop:
+      return 2
+    case .rightBottom:
+      return 2
+    }
+  }
+  
+  var dy: CGFloat {
+    switch self {
+    case .leftTop:
+      return -2
+    case .leftBottom:
+      return 2
+    case .rightTop:
+      return -2
+    case .rightBottom:
+      return 2
+    }
+  }
 }
