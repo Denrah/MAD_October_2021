567d7aaaf99d471b488fb55ccbb3f05a433aa17e
Denis Sharapov <denisharapov@yandex.ru>
2021-11-01 16:19:02 +0700

    minor fix

--- TrainTest1/App/ViewController.swift
+++ TrainTest1/App/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     
     if UserDefaults.standard.string(forKey: "access") != nil {
-      navigationController?.setViewControllers([AboutMeViewController()], animated: false)
+      navigationController?.setViewControllers([MainViewController()], animated: false)
     } else {
       navigationController?.setViewControllers([StartViewController()], animated: false)
     }
